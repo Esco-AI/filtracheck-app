d0b5391962d7846a14c34f984de0b53126143003
Muhammad Fadhil Hidayatullah <fgeminni0@gmail.com>
2025-09-22 15:48:48 +0700

    feat: refine add chemical screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'home/screens/home_screen.dart';
 
 void main() {
@@ -12,6 +13,28 @@ class FiltraCheckApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          surfaceTintColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+
+          foregroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.white),
+          actionsIconTheme: IconThemeData(color: Colors.white),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w800,
+            fontSize: 20,
+          ),
+
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        ),
+      ),
+
       home: const HomeScreen(),
     );
   }
